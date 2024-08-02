2dc7991041bea925d52ce005fd4d6e15f984f0f3
Max-GO-IT <terletskymaxim@ukr.net>
2024-08-02 23:38:06 +0300

    ...

--- vite-project/src/components/FriendListItem.jsx
+++ vite-project/src/components/FriendListItem.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const FriendListItem = ({ avatar, name, isOnline }) => {
     return (
       <div>
-        <img src={avatar} alt="Avatar" width="48" />
+        <img  src={avatar} alt="Avatar" width="48" />
         <p>{name}</p>
         <p style ={{ color: isOnline ? 'green' : 'red' }}>{isOnline ? "Online" : "Offline"}</p>
       </div>
